0ade087c1c19552fac6d580cec20a412182c198b
Hants Williams <hantsawilliams@gmail.com>
2025-08-27 21:33:54 -0400

    added some more files for the end of class

--- Module1_MedicalCodexes/example.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd 
-
-datafile1 = pd.read_csv("datafile1.csv")
-datafile2 = pd.read_csv("datafile2.csv")
---
+++ Module1_MedicalCodexes/icd/icd_10_cm.py
@@ -0,0 +1,3 @@
+import pandas as pd 
+
+icd10cm = pd.read_csv('./Module1_MedicalCodexes/icd/icd10cm_order_2025.txt', sep="\t", header=None)
