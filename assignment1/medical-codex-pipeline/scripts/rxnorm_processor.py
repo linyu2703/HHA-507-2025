1040e2ce743581ec8e9326403614faacc7891b08
YU LIN <118993869+linyu2703@users.noreply.github.com>
2025-09-10 14:32:28 -0400

    update

--- assignment1/medical-codex-pipeline/scripts/rxnorm_processor.py
+++ assignment1/medical-codex-pipeline/scripts/rxnorm_processor.py
@@ -1,11 +1,17 @@
 import polars as pl
 from pathlib import Path
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from utils.common_functions import save_to_format
 
 #need to fix + change directory
 
 # https://www.nlm.nih.gov/research/umls/rxnorm/docs/techdoc.html#s12_10
 
-file_path = Path('Module1_MedicalCodexes/rxnorm/files/RXNATOMARCHIVE.RRF')
+file_path = Path('input/RXNATOMARCHIVE.RRF')
 
 columns = [
     'rxaui', 'aui', 'str', 'archive_timestamp', 'created_timestamp', 
@@ -21,14 +27,40 @@ df = pl.read_csv(
     truncate_ragged_lines=True
 )
 
-output_dir = Path('Module1_MedicalCodexes/rxnorm/output')
-output_dir.mkdir(exist_ok=True)
-output_path = output_dir / 'RXNATOMARCHIVE.csv'
+df_col = ['code', 'str']
 
-df.write_csv(output_path)
+df_small = df[['code', 'str']]
+
+df_small = df[df_col]
+
+df_small = df_small.rename({'str': 'description'})
+
+
+#trimming
+df_small["description"].str.strip_chars().str.replace_all(r"\s+", " ").alias("description")
+
+
+# #missing + null values
+# df_small = df_small.replace({'': None, 'nan': None, 'NaN': None}).alias(df_small)
+# df_small = df_small.dropna(subset=['code', 'description']) #now there is no empty description/codes
+
+df_small = df_small.unique(subset=['code'])
+
+df_small = df_small.with_columns([
+    pl.lit("09-10-2025").alias("last_updated")
+])
+
+print(df_small.head())
+
+save_to_format(df_small, baseFile="RXNATOMARCHIVE")
+
+# output_dir = Path('Module1_MedicalCodexes/rxnorm/output')
+# output_dir.mkdir(exist_ok=True)
+# output_path = output_dir / 'RXNATOMARCHIVE.csv'
+
+# df.write_csv(output_path)
 
 print(f"Successfully parsed {len(df)} records from RXNATOMARCHIVE.RRF")
-print(f"Saved to {output_path}")
 print(f"Dataset shape: {df.shape}")
 print(f"\nFirst 5 rows:")
 print(df.head())
--- assignment1/medical-codex-pipeline/scripts/snomed_processor.py
+++ assignment1/medical-codex-pipeline/scripts/snomed_processor.py
@@ -1,9 +1,14 @@
 import polars as pl
 from pathlib import Path
+import os
+import sys
 
-# need to fix + change directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-file_path = Path('Module1_MedicalCodexes/snowmed/sct2_Description_Full-en_US1000124_20250301.txt')
+from utils.common_functions import save_to_format
+
+
+file_path = Path('input/sct2_Description_Full-en_US1000124_20250301.txt')
 
 df = pl.read_csv(
     file_path,
@@ -25,14 +30,41 @@ df = pl.read_csv(
     }
 )
 
-output_dir = Path('Module1_MedicalCodexes/snowmed/output')
-output_dir.mkdir(exist_ok=True)
-output_path = output_dir / 'sct2_Description_Full.csv'
+df_col = ['conceptId', 'term']
 
-df.write_csv(output_path)
+df_small = df[['conceptId', 'term']]
+
+df_small = df[df_col]
+
+df_small = df_small.rename({'conceptId': 'code', 'term': 'description'})
+
+print(df_small.head())
+
+#trimming
+df_small["description"].str.strip_chars().str.replace_all(r"\s+", " ").alias("description")
+
+
+# #missing + null values
+# df_small = df_small.replace({'': None, 'nan': None, 'NaN': None}).alias(df_small)
+# df_small = df_small.dropna(subset=['code', 'description']) #now there is no empty description/codes
+
+df_small = df_small.unique(subset=['code'])
+
+df_small = df_small.with_columns([
+    pl.lit("09-10-2025").alias("last_updated")
+])
+
+print(df_small.head())
+
+save_to_format(df_small, baseFile="sct2_Description_Full")
+
+# output_dir = Path('Module1_MedicalCodexes/snowmed/output')
+# output_dir.mkdir(exist_ok=True)
+# output_path = output_dir / 'sct2_Description_Full.csv'
+
+# df.write_csv(output_path)
 
 print(f"Successfully parsed {len(df)} records from SNOMED CT file")
-print(f"Saved to {output_path}")
 print(f"Dataset shape: {df.shape}")
 print(f"\nColumn names: {df.columns}")
 print(f"\nFirst 5 rows:")
