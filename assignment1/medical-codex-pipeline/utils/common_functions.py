ee3c3382e71d773832a185f37a06d04923de8c7b
YU LIN <118993869+linyu2703@users.noreply.github.com>
2025-09-09 20:36:43 -0400

    udapte

--- assignment1/medical-codex-pipeline/utils/common_functions.py
+++ assignment1/medical-codex-pipeline/utils/common_functions.py
@@ -2,8 +2,6 @@ import pandas as pd
 import os
 
 def save_to_format(df:pd.DataFrame, baseFile: str, outputDir: str='output') -> None:
-    if outputDir is None:
-        outputDir = os.path.join(os.path.dirname(__file__), '..', 'output')
     os.makedirs(outputDir,exist_ok=True)
     csv_path = os.path.join(outputDir, f"{baseFile}.csv")
     df.to_csv(csv_path, index = False)
